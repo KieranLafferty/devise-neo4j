e05e5dd01a78680db8f08620e1eee698a2127f2a
Kieran Lafferty <kieran.lafferty@gmail.com>
2014-04-29 21:49:09 -0700

    test

--- lib/devise/orm/neo4j.rb
+++ lib/devise/orm/neo4j.rb
@@ -1,5 +1,5 @@
 require 'devise-neo4j'
-require 'orm_adapter/adapters/neo4j'
+# require 'orm_adapter/adapters/neo4j'
 require 'devise/orm/neo4j/date_time'
 require 'devise/orm/neo4j/compatibility'
 require 'devise/orm/neo4j/nil_class_converter'
